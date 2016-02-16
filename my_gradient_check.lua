691c8d171412db589b96ae88be7806f852b36c4f
Tudor Berariu <tudor.berariu@gmail.com>
2016-02-16 17:41:27 +0200

    Added some comments

--- my_gradient_check.lua
+++ my_gradient_check.lua
@@ -9,34 +9,42 @@ require("nn")
 --- Do gradient check for a given model, criterion and a given test case
 --------------------------------------------------------------------------------
 
-function gradientCheck(model, criterion, testInputs, testOutputs, epsilon)
-   w, dldw = model:getParameters()
-
-   n = w:size(1)
+function gradientCheck(model, criterion, inputs, outputs, epsilon)
+   local W, dJdW = model:getParameters()              -- parametes and gradients
+   local n = W:size(1)                               -- the number of parameters
 
    --- 1. Compute gradient (backpropagation)
-   dldw:zero()
-   criterion:forward(model:forward(testInputs), testOutputs)
-   model:backward(testInputs, criterion:backward(model.output, testOutputs))
+   dJdW:zero()                                                -- erase gradients
+   criterion:forward(model:forward(inputs), outputs)                  -- forward
+   model:backward(inputs, criterion:backward(model.output, outputs))  --backward
 
-   origGradient = dldw:clone()
-   origW = w:clone()
+   local origGradient = dJdW:clone()                       -- save the gradients
+   local origW = W:clone()                                -- save the parameters
 
    --- 2. Compute gradient (using definition)
-   compGradient = torch.Tensor(n)
-   compGradient:zero()
+   local defGradient = torch.Tensor(n)                    -- definition gradient
+   defGradient:zero()                                            -- make it zero
 
-   for i=1,n do
-      w:copy(origW)
-      w[i] = w[i] + epsilon
-      j1 = criterion:forward(model:forward(testInputs), testOutputs)
+   for i=1,n do                                         -- for the ith parameter
+      W:copy(origW)               -- take the original values for the parameters
+      W[i] = W[i] + epsilon                       -- slightly modify the ith one
+      local j1 = criterion:forward(model:forward(inputs), outputs)     -- loss 1
 
-      w[i] = w[i] - 2 * epsilon
-      j2 = criterion:forward(model:forward(testInputs), testOutputs)
+      W[i] = W[i] - 2 * epsilon        -- slightly modify in the other direction
+      local j2 = criterion:forward(model:forward(inputs), outputs)     -- loss 2
 
-      compGradient[i] = (j2 - j1) / (2 * epsilon)
+      defGradient[i] = (j1 - j2) / (2 * epsilon)               -- compute dJdW_i
    end
-   print(torch.cat(compGradient, origGradient, 2))
+
+   local distance = torch.norm(defGradient - origGradient)
+   if distance < 1e-10 then
+      print("Ok!")
+   else
+      print("Not Ok!")
+   end
+   print("Distance: " .. distance)
+
+   --print(torch.cat(compGradient, origGradient, 2))
 end
 
 
