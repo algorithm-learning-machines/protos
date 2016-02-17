add1ba6afa30ec7ff8a28747427647dcd221ebda
Tudor Berariu <tudor.berariu@gmail.com>
2016-02-17 14:35:36 +0200

    Refactorized Q-Learning a bit

--- reinforcement-learning/QPlayer.lua
+++ reinforcement-learning/QPlayer.lua
@@ -11,33 +11,37 @@ require("torch")
 local QPlayer = {}
 QPlayer.__index = QPlayer
 
-function QPlayer.create(Game, opt)
-   self.{}
+function QPlayer.create(Game, opt)                       -- intialize new player
+   self = {}
    setmetatable(self, QPlayer)
-   self.Q = {}
+   self.Q = {}                    -- initalize a table for (state-action) values
+
    self.epsilon = opt.epsilon or 0.1
    self.learningRate = opt.learningRate or 0.1
    self.discount = opt.discount or 0.99
-   self.actionsNo = #(Game.getActions())
+
+   self.actionsNo = #(Game.getActions())           -- how many actions available
    return self
 end
 
 function QPlayer:move(state, isTraining)
-   local stateStr = state:serialize()
-   local actions = self.Q[stateStr] or {}
-   local bestAction = torch.random(self.actionsNo)
-   local bestQ = actions[bestAction] or 0
+   local stateStr = state:serialize()                -- serialized state is key
+   local actions = self.Q[stateStr] or {}           -- knowns state-value pairs
+   local bestAction
 
    if (not isTraining) or (torch.rand(1)[1] >= self.epsilon) then
       local idxs = torch.randperm(self.actionsNo)
-      for i = 1, #(self.actionsNo) do
+      local bestQ = nil
+      for i = 1, self.actionsNo do
          if not actions[idxs[i]] then      -- if there is an unexplored action
             return idxs[i]
-         elseif actions[idxs[i]] > bestQ then
+         elseif (not bestQ) or (actions[idxs[i]] > bestQ) then
             bestQ = actions[idxs[i]]
             bestAction = idxs[i]
          end
       end
+   else
+      bestAction = torch.random(self.actionsNo)
    end
    return bestAction
 end
@@ -47,14 +51,28 @@ function QPlayer:feedback(state, action, reward, nextState)
    local nextStateStr = nextState:serialize()
    self.Q[stateStr] = self.Q[stateStr] or {}
    local actions = self.Q[stateStr] or {}
-   local oldValue = 0 or actions[action]
+   local oldValue = actions[action]
 
    local nextActions = self.Q[nextStateStr] or {}
-   local maxNext = 0
-   for _,q in pairs(nextActions) do
-      if q > maxNext then maxNext = ql end
+   if nextState:isFinal() then
+      if oldValue then
+         self.Q[stateStr][action] = (1-self.learningRate) * oldValue +
+            self.learningRate * reward
+      else
+         self.Q[stateStr][action] = self.learningRate * reward
+      end
+   else
+      local maxNext = 0
+      for _, q in pairs(nextActions) do
+         if q > maxNext then maxNext = q end
+      end
+      if oldValue then
+         self.Q[stateStr][action] = (1-self.learningRate) * oldValue +
+            self.learningRate * (reward + self.discount * maxNext)
+      else
+         self.Q[stateStr][action] = reward + self.discount * maxNext
+      end
    end
-
-   self.Q[stateStr][action] = (1-self.learningRate) * oldValue +
-      self.learningRate * (reward + self.discount * maxNext)
 end
+
+return QPlayer
