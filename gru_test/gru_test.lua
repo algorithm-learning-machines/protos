e1afc6bfd42ac823de0dadcf9b7fc1a01b6eaebc
Zappaman <iosifadrianmihai@gmail.com>
2016-04-12 01:29:51 +0300

    added feval

--- gru_test/gru_test.lua
+++ gru_test/gru_test.lua
@@ -1,31 +1,69 @@
 require 'torch'
 require 'nn'
 require 'rnn'
+require 'optim'
 
 local data_loader = require("data_loader")
 data_loader:__init(10, 20, 10)
 
-local my_gru =  nn.LSTM(data_loader.v_size + data_loader.address_size,
+local model=  nn.GRU(data_loader.v_size + data_loader.address_size,
                         data_loader.address_size)
-local mse = nn.MSECriterion()
-for i=1,1000 do
+local criterion = nn.MSECriterion()
+
+params, grad_params = model:getParameters()
+params:zero()
+config = {learningRate = 1e-5, momentum = 0.9}
+dataset = {}
+function dataset:size() return 1000 end
+for i=1, dataset:size() do
    local x, t = data_loader:getNext()
-   local out = my_gru:forward(x)
-   local err = mse:forward(out, t)
-   local d_err = mse:backward(out, t)
-   my_gru:backward(x, d_err)
-   --print(err)
+   dataset[i] = {x, t}
 end
+
+
+function train()
+    for t=1,dataset:size() do
+        local feval = function(x)
+            if x ~= params then
+                params:copy(x)
+            end
+
+            grad_params:zero()
+
+            local input = dataset[t][1]
+            local target = dataset[t][2]
+
+            local output = model:forward(input)
+            local err = criterion:forward(output, target)
+
+            local dfdo = criterion:backward(output, target)
+
+            model:backward(input, dfdo)
+            grad_params:div(input:size(1))
+
+            return err, grad_params
+        end
+
+        optim.adam(feval, params, config)
+        print("finished sgd "..t)
+    end
+end
+for j = 1,10 do
+    train()
+end
+
+
+print("...training")
+eval_eps = 100
 for i=1,100 do
-    local x, t = data_loader:getNext(true)
-   local out = my_gru:forward(x)
-   local err = mse:forward(out, t)
-   local d_err = mse:backward(out, t)
-   print("OUT-------")
-   print(out)
-   print("TEST------")
-   print(t)
-   print("END-------")
-   my_gru:backward(x, d_err)
+   local x, t = data_loader:getNext(true)
+   local out = model:forward(x)
+   local err = criterion:forward(out, t)
+   _, ix_out = out:max(1)
+   _, ix_t = t:max(1)
+   if ix_out[1] ~= ix_t[1] then
+       eval_eps = eval_eps - 1
+   end
 end
+print(eval_eps)
 --print(data_loader:getNext())
